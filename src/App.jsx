58c8a879372524898da9d807fb5b15ab260e456d
khushi singh <khushikhushi89978@gmail.com>
2025-11-04 01:27:51 +0530

    macbook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Hero from './components/Hero'
 import { Navvar } from './components/Navvar'
+import { ProductViewer } from './components/ProductViewer'
 import './index.css'
+import gsap from 'gsap';
+import {ScrollTrigger,SplitText} from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   
@@ -9,6 +14,7 @@ function App() {
     <>
      <Navvar/>
      <Hero/>
+     <ProductViewer/>
     </>
   )
 }
